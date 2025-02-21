3863f6f87018f6811aedd020b3bd04f744363d4e
Jose23Tombe <168827206+Jose23Tombe@users.noreply.github.com>
2025-02-20 19:05:21 -0500

    Builder.php

--- Builder.php
+++ Builder.php
@@ -1,11 +1,12 @@
 <?php
-
+// Clase principal que representa el objeto a construir
 class Auto {
     private $marca;
     private $modelo;
     private $año;
     private $color;
 
+// Constructor que recibe los valores y los asigna a los atributos
     public function __construct($marca, $modelo, $año, $color) {
         $this->marca = $marca;
         $this->modelo = $modelo;
@@ -13,42 +14,48 @@ class Auto {
         $this->color = $color;
     }
 
+// Metodo magico que permite convertir el objeto en una cadena de texto
     public function __toString() {
         return "Auto: $this->marca $this->modelo ($this->año), Color: $this->color";
     }
 }
 
+// Clase Builder que ayuda a construir objetos Auto
 class AutoBuilder {
     private $marca;
     private $modelo;
     private $año;
     private $color;
 
+// Metodo para establecer la marca del auto
     public function setMarca($marca) {
         $this->marca = $marca;
         return $this;
     }
 
+// Metodo para establecer el modelo del auto
     public function setModelo($modelo) {
         $this->modelo = $modelo;
         return $this;
     }
 
+// Metodo para establecer el año del auto
     public function setAño($año) {
         $this->año = $año;
         return $this;
     }
 
+// Metodo para establecer el color del auto  
     public function setColor($color) {
         $this->color = $color;
         return $this;
     }
-
+// Metodo que construye y devuelve un objeto Auto
     public function build() {
         return new Auto($this->marca, $this->modelo, $this->año, $this->color);
     }
 }
-
+// Uso del Builder para construir un auto
 $auto1 = (new AutoBuilder())
     ->setMarca('Toyota')
     ->setModelo('TXL')
@@ -56,13 +63,17 @@ $auto1 = (new AutoBuilder())
     ->setColor('Rojo')
     ->build();
 
+// Creacion de otro auto con valores diferentes
 $auto2 = (new AutoBuilder())
     ->setMarca('Ford')
     ->setModelo('Ranger')
     ->setAño(2023)
-    ->setColor('Negro')
+    ->setColor('Beige')
     ->build();
 
+
+//resultados
 echo $auto1 . "<br>";
 echo $auto2 . "<br>"
 ?>
+
